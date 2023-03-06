173cfc05ec26e72f17343102796c237f9b79205a
DaCoDeMaNiaK <jla.webprojet@gmail.com>
2023-03-06 11:03:40 +0100

    Create script in package Using let and const keyword

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+/**
+ * main.ts
+ * @author Aélion <jean-luc.aubert@aelion.fr>
+ * @version 1.0.0
+ * 
+ * Entry point of our frontend application
+ */
+let myName: string
+myName = 'Jean-Luc'
+
+const myLastName: string = 'Aubert'
+
+
+console.log(myName)
